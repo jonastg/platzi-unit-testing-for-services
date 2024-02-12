1055dbf9b37fb09607ce0330f6d45fc19f01413c
Jonas Talavera <jonastg@gmail.com>
2024-02-12 20:52:48 +0100

    add mocha report and basic tests

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,17 +1,35 @@
 import { Calculator } from './calculator';
 
 describe('Test for Calculator', () => {
-  it('3x3 should return 9', () => {
-    const calculator = new Calculator();
-    const rta = calculator.multiply(3, 3);
+  describe('Tests for multiply operation', () => {
+    it('3x3 should return 9', () => {
+      const calculator = new Calculator();
+      const rta = calculator.multiply(3, 3);
 
-    expect(rta).toEqual(9);
+      expect(rta).toEqual(9);
+    });
+
+    it('1x4 should return 4', () => {
+      const calculator = new Calculator();
+      const rta = calculator.multiply(1, 4);
+
+      expect(rta).toEqual(4);
+    });
   });
 
-  it('divide by 0 should return null', () => {
-    const calculator = new Calculator();
-    const rta = calculator.divide(3, 0);
+  describe('Tests for divide operation', () => {
+    it('should return a some numbers', () => {
+      const calculator = new Calculator();
 
-    expect(rta).toBeNull();
+      expect(calculator.divide(6, 3)).toEqual(2);
+      expect(calculator.divide(5, 2)).toEqual(2.5);
+    });
+
+    it('divide by 0 should return null', () => {
+      const calculator = new Calculator();
+      const rta = calculator.divide(3, 0);
+
+      expect(rta).toBeNull();
+    });
   });
 });
